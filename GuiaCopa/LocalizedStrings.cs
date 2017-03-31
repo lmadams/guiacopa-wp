eaa67bd7efe7a8526352f1c0c0fe2e7531884adf
Lucas Adams <adams.lucass@gmail.com>
2017-03-30 22:11:37 -0300

    Primeiro commit

---
+++ GuiaCopa/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GuiaCopa.Resources;
+
+namespace GuiaCopa
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
